07327dbcf11e09b0f17fb6166bf66894e8cfce19
zhinanai <664895328@qq.com>
2018-09-05 11:05:35 +0800

    no message

--- shop/Home/Controller/QuestionController.class.php
+++ shop/Home/Controller/QuestionController.class.php
@@ -11,16 +11,6 @@ class QuestionController  extends CommonController{
     private function getQustion(){
         return new Question();
     }
-    /*
-     * 问答大厅
-     * */
-    public function index(){
-        if(IS_GET) {
-            echo 11;
-        }elseif(IS_POST){
-            echo 22;
-        }
-    }
 
     /*
      * 我的所有问答
@@ -91,11 +81,67 @@ class QuestionController  extends CommonController{
     /* 问题列表 */
     public function questionlist()
     {
-        $this->display();
+        if(IS_GET) {
+            $this->display();
+        }else{
+
+        }
     }
     /* 搜索 */
     public function search()
     {
         $this->display();
+    }
+
+    /*
+     * 提问
+     * */
+    public function report(){
+        if(IS_GET) {
+            return $this->display();
+        }elseif(IS_POST){
+           $questionArr=I("post.");
+          $this->getImgPath($questionArr['content']);
+        }
+    }
+
+    /*
+     * 匹配Img标签并替换标签路径内容
+     * */
+    public function getImgPath($str){
+        $imgpreg = 'src="(.*?)">';
+        preg_match($imgpreg,$str,$img);
+        var_dump($img);
+        $mycount=count($img)-1;
+        $imgval = $img[$mycount];
+        if(!empty($imgval)){
+            echo $imgval;
+        }else{
+            echo 'no';
+        }
+    }
+
+    /*
+     * 转换图片
+     * return imgPath 图片保存路径
+     * */
+    public function base64ToImg($base64){
+
+        if(strpos($base64,"data:image/png;base64,")>-1){
+            $str="data:image/png;base64,";
+            $ext="png";
+        }elseif(strpos($base64,"data:image/jpg;base64,")>-1){
+            $str="data:image/jpg;base64,";
+            $ext="jpg";
+        }elseif(strpos($base64,"data:image/gif;base64,")>-1){
+            $str="data:image/gif;base64,";
+            $ext="gif";
+        }
+        $imgStr=base64_decode(str_replace($str,"",$base64));//解码图片为二进制流
+        $filePath="/question/".date("Y-m-d",time());//文件保存目录
+        $fileName=md5(time()+rand(1,100000));//md5随机加密
+        $imgPath=$filePath."/$fileName.".$ext;//完整图片路径
+        file_put_contents($imgPath,$imgStr);//写入文件
+        return $imgPath;
     }
 }
